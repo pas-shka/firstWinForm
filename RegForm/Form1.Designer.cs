372b82f32817001eecf5e55a10c7acfefceea5b5
Pavel <Pavel@PAS-SHKA>
2019-05-28 23:00:12 +0300

    all proj

---
+++ RegForm/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace RegForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.passBox1 = new System.Windows.Forms.TextBox();
+            this.loginBox1 = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.loginBox = new System.Windows.Forms.TextBox();
+            this.signUp = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lastNameBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(232, 275);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.passBox1);
+            this.tabPage1.Controls.Add(this.loginBox1);
+            this.tabPage1.Controls.Add(this.loginButton);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(224, 249);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Sign In";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Login";
+            // 
+            // passBox1
+            // 
+            this.passBox1.Location = new System.Drawing.Point(41, 131);
+            this.passBox1.Name = "passBox1";
+            this.passBox1.Size = new System.Drawing.Size(140, 20);
+            this.passBox1.TabIndex = 5;
+            // 
+            // loginBox1
+            // 
+            this.loginBox1.Location = new System.Drawing.Point(41, 73);
+            this.loginBox1.Name = "loginBox1";
+            this.loginBox1.Size = new System.Drawing.Size(140, 20);
+            this.loginBox1.TabIndex = 4;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(71, 191);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 0;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.lastNameBox);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.nameBox);
+            this.tabPage2.Controls.Add(this.passBox);
+            this.tabPage2.Controls.Add(this.loginBox);
+            this.tabPage2.Controls.Add(this.signUp);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(224, 249);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Sign Up";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(67, 93);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(136, 20);
+            this.nameBox.TabIndex = 5;
+            // 
+            // passBox
+            // 
+            this.passBox.Location = new System.Drawing.Point(67, 67);
+            this.passBox.Name = "passBox";
+            this.passBox.Size = new System.Drawing.Size(136, 20);
+            this.passBox.TabIndex = 4;
+            // 
+            // loginBox
+            // 
+            this.loginBox.Location = new System.Drawing.Point(67, 41);
+            this.loginBox.Name = "loginBox";
+            this.loginBox.Size = new System.Drawing.Size(136, 20);
+            this.loginBox.TabIndex = 3;
+            // 
+            // signUp
+            // 
+            this.signUp.Location = new System.Drawing.Point(67, 194);
+            this.signUp.Name = "signUp";
+            this.signUp.Size = new System.Drawing.Size(75, 23);
+            this.signUp.TabIndex = 2;
+            this.signUp.Text = "Sign Up";
+            this.signUp.UseVisualStyleBackColor = true;
+            this.signUp.Click += new System.EventHandler(this.signUp_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Login";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Password";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Name";
+            // 
+            // lastNameBox
+            // 
+            this.lastNameBox.Location = new System.Drawing.Point(67, 119);
+            this.lastNameBox.Name = "lastNameBox";
+            this.lastNameBox.Size = new System.Drawing.Size(136, 20);
+            this.lastNameBox.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Last Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 441);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox passBox1;
+        private System.Windows.Forms.TextBox loginBox1;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox passBox;
+        private System.Windows.Forms.TextBox loginBox;
+        private System.Windows.Forms.Button signUp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox lastNameBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ RegForm/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegForm
+{
+    public partial class Form1 : Form
+    {
+        List<Person> user = new List<Person>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void signUp_Click(object sender, EventArgs e)
+        {
+            user.Add(new Person() { Login = loginBox.Text, Password = passBox.Text, Name=nameBox.Text});
+            
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+                foreach (Person person in user)
+                {
+                    if (loginBox1.Text == person.Login && passBox1.Text == person.Password)
+                    {
+                        MessageBox.Show("Welcome to App");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Baaad job");
+                    }
+                }                       
+        }
+    }
+}
